2e325098f67a985f5af20041ee853358cb642d60
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 11:31:38 -0300

    check-point-front-01

---
+++ script.js
@@ -0,0 +1,23 @@
+let nome = 'Micael Santos azarias';
+    console.log(nome);
+let idadeAluno = 22;
+    console.log(idadeAluno);
+let listaTimes = ['palmeiras', 'santos', 'cuiaba'] 
+    console.log(listaTimes);
+let status = true 
+    console.log(status);
+let valorProduto = 10.50
+    console.log(valorProduto);
+
+let num1 = 10
+let num2 = 20
+
+let soma = num1 + num2;
+let subtracao= num1- num2;
+let multiplicacao = num1 * num2;
+let divisao = num1/ num2;
+
+console.log("O resultado da soma é: "+ soma);
+console.log("O resultado da subtração é: "+ subtracao);
+console.log("O resultado da multiplicação é: " + multiplicacao);
+console.log("O resultado da divisão é: "-+ divisao);
